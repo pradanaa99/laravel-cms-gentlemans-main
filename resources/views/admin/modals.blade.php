d7c026308b15c5cef8ad104961af344359b92efb
pradanaa99 <yudhaputrapradana@gmail.com>
2023-06-14 15:39:00 +0700

    cms

---
+++ resources/views/admin/modals.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.admin')
+@section('content')
+<div class="container grid px-6 mx-auto">
+    <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+        Modals
+    </h2>
+    <div class="max-w-2xl px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
+        <p class="mb-4 text-gray-600 dark:text-gray-400">
+            This is possibly
+            <strong>the most accessible a modal can get</strong>
+            , using JavaScript. When opened, it uses
+            <code>assets/js/focus-trap.js</code>
+            to create a
+            <em>focus trap</em>
+            , which means that if you use your keyboard to navigate around,
+            focus won't leak to the elements behind, staying inside the
+            modal in a loop, until you take any action.
+        </p>
+
+        <p class="text-gray-600 dark:text-gray-400">
+            Also, on small screens it is placed at the bottom of the screen,
+            to account for larger devices and make it easier to click the
+            larger buttons.
+        </p>
+    </div>
+
+    <div>
+        <button @click="openModal"
+            class="px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+            style="background-color: rgb(22 163 74)">
+            Open Modal
+        </button>
+    </div>
+</div>
+@endsection
---
+++ resources/views/landingpages/landingpage.blade.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gentleman's Indonesia</title>
+    <link rel="stylesheet" href="assets/css/landingpage.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+</head>
+<body>
+    <div class="container">
+        <div class="topbar">
+            <img src="assets/mainlogo.png" width="100%" alt="">
+            <div class="hidden-component">
+                <ul class="menutopbar">
+                    <center>
+                        <li>CASUAL</li>
+                        <li>LUXURY</li>
+                        <li>SPORTS</li>
+                        <li>BAG</li>
+                        <li class="dropdown-catalogue">CATALOGUE
+                            {{-- <li>SHIRT</li> 
+                            <li>JACKET</li>
+                            <li>HOODIE</li>
+                            <li>PANTS</li>
+                            <li>SHORTS</li>--}}
+                        </li>
+                        <li>ABOUT</li>
+                        
+                    </center>
+                </ul>
+            </div>
+            <div class="search-box">
+                <button class="btn-search"><i class="fas fa-search"></i></button>
+                <input type="text" class="input-search" placeholder="WHAT ARE YOU LOOKING FOR?">
+            </div>
+            <i class="fas fa-shopping-cart"></i>
+        </div>
+        <div class="hero-slider">
+            <div class="heroimg1">
+                <center><img src="assets/HERO.jpg" width="100%" alt="Gambar 1"></center>
+                <br>
+                <center><P>JUN 23</P></center>
+                <span>GET UP TO 50% SPECIAL SALE DISCOUNT !</span>
+            </div>
+        </div>
+        
+        <div class="content">
+            <div class="heading-title">
+                <h1><i>Get your best appearance with us!</i></h1>
+            </div>
+            <br><br><br><br><br><br>
+            <div class="mini-gallery">
+                <center>
+                    <div class="poster1">
+                        <img src="assets/mini-gallery/poster1.jpg" alt="cover" width="100%">
+                    </div>
+                    <div class="poster2">
+                        <img src="assets/mini-gallery/poster2.jpg" alt="" width="100%">
+                    </div>
+                    <div class="poster3">
+                        <img src="assets/mini-gallery/poster3.jpg" alt="" width="100%">
+                    </div>
+                </center>
+            </div>
+            <div class="mini-gallery2">
+                <center>
+                    <div class="poster4">
+                        <img src="assets/mini-gallery/poster4.jpg" alt="cover" width="100%">
+                    </div>
+                    <div class="poster5">
+                        <img src="assets/mini-gallery/poster5.jpg" alt="" width="100%">
+                    </div>
+                    <div class="poster6">
+                        <img src="assets/mini-gallery/poster6.jpg" alt="" width="100%">
+                    </div>
+                </center>
+            </div>
+            <br><br><br><br><br>            
+            <p>New Releases</p>
+            <br><br><br>
+            <hr>
+            <br><br><br>
+            <div class="newcatalogue">
+                <div class="cat1">
+                    <img src="assets/newreleases/new1.jpg" alt="cover" width="100%">
+                    <center>BLACK SHIRT</center>
+                    <div class="cat1price">
+                        <center>IDR. 199K</center>
+                    </div>
+                </div>
+                <div class="cat2">
+                    <img src="assets/newreleases/new2.jpg" alt="" width="100%">
+                    <center>GRAFFITI SHIRT OVERSIZED</center>
+                    <div class="cat2price">
+                        <center>IDR. 299K</center>
+                    </div>
+                </div>
+                <div class="cat3">
+                    <img src="assets/newreleases/new3.jpg" alt="" width="100%">
+                    <center>COCOON SHIRT</center>
+                    <div class="cat3price">
+                        <center>IDR. 399K</center>
+                    </div>
+                </div>
+            </div>
+            <div class="newcatalogue2">
+                <div class="cat4">
+                    <img src="assets/newreleases/new4.jpg" alt="" width="100%">
+                    <center>LOGO STRIPS SHIRT</center>
+                    <div class="cat4price">
+                        <center>IDR. 699K</center>
+                    </div>
+                </div>
+                <div class="cat5">
+                    <img src="assets/newreleases/new5.jpg" alt="" width="100%">
+                    <center>BLACK PADDED SHIRT</center>
+                    <div class="cat5price">
+                        <center>IDR. 499K</center>
+                    </div>
+                </div>
+                <div class="cat6">
+                    <img src="assets/newreleases/new6.jpg" alt="" width="100%">
+                    <center>SHORT SLEEVE MIRROR BLACK SHIRT</center>
+                    <div class="cat6price">
+                        <center>IDR. 599K</center>
+                    </div>
+                </div>
+            </div>
+            <br><br><br><br><br><br><br><br><hr><br><br>
+        </div>
+        <div class="bestsellerimg">
+            <center><img src="assets/HERO2.jpg" alt="ladiesfav" width="100%"></center>
+            <br>
+            <span class="ladies-caption">Ladies's Fav Coming Soon !</span>
+        </div>
+        <div class="content2">
+            <hr>
+            <h2>Boost your Confidence</h2>
+            <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, iusto quam provident doloremque dolorem quas iste temporibus deleniti sunt! Quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum rerum est odit. Voluptates recusandae saepe exercitationem. Debitis, suscipit. Nesciunt, incidunt?</p>
+            <br><br><br><br><br><br><br><br><hr><br><br>
+        </div>
+    </div>
+    <footer>
+        <center>
+            2023, Gentleman's E-Commerce. All rights reserved.
+        </center> 
+    </footer>
+</body>
+</html>
